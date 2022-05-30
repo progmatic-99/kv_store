1e76ea892bf765d0ef87973558c5b968e286ed38
Shubham Upreti <shubham.upreti@gmail.com>
2022-05-30 23:18:23 +0530

    db created

---
+++ README.md
@@ -0,0 +1,9 @@
+## Distributed Key Value store
+
+### Master server
+- Keeps track of the metadata of the data
+- Stores keys
+
+
+### Volume server
+- Stores the actual data
--- src/server.py
+++ src/server.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+
+if os.environ["TYPE"] == "master":
+    import plyvel
+
+    db = plyvel.DB(os.environ["DB"], create_if_missing=True)
+
+
 def master(env, start_response):
     print(env)
     start_response("200 OK", [("Content-Type", "text/html")])
