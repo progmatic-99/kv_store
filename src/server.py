8e9a9e6947239373e5b913fdd04e0e21cb3de626
Shubham Upreti <shubham.upreti@gmail.com>
2022-06-01 00:09:42 +0530

    db created

--- src/server.py
+++ src/server.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import time
+import hashlib
 
 
 if os.environ["TYPE"] == "master":
@@ -9,9 +11,25 @@ if os.environ["TYPE"] == "master":
 
 
 def master(env, start_response):
-    print(env)
-    start_response("200 OK", [("Content-Type", "text/html")])
-    return [b"Hello World"]
+    key = env["REQUEST_URI"].encode("utf-8")
+
+    metakey = db.get(key)
+    if metakey is None:
+        # key doesn't exist
+        start_response("404 Not found", [("Content-Type", "text/html")])
+        return [b"key not found"]
+
+    # key found: 'volume'
+    meta = json.loads(metakey)
+
+    # send the redirect
+    headers = [("location", "http://%s%s" % (meta["volume"], key)), ("expires", "0")]
+    start_response("302 Found", headers)
+    return [b""]
+
+
+if os.environ["TYPE"] == "volume":
+    host = socket.gethostname()
 
 
 def volume(env, start_response):
