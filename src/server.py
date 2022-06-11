2e8b18578e1e6a4e499e0e0cda8ad3f0c9df127a
Shubham Upreti <shubham.upreti@gmail.com>
2022-06-11 23:31:21 +0530

    changes to master server

--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
 import os
+import json
 import sys
 import time
 import hashlib
@@ -21,39 +22,41 @@ if os.environ["TYPE"] == "master":
 
 
 def master(env, start_response):
-    key = env["REQUEST_URI"].encode("utf-8")
+    key = env["REQUEST_URI"]
+    metakey = db.get(key.encode("utf-8"))
 
-    metakey = db.get(key)
     if metakey is None:
         if env["REQUEST_METHOD"] == "PUT":
             # handle put requests
             volume = random.choice(volumes)
 
-            json.dumps({"volume": volume}).encode("utf-8")
-            db.put(key, metakey)
+            meta = {"volume": volume}
+            db.put(key.encode("utf-8"), json.dumps(meta).encode("utf-8"))
         else:
             # key doesn't exist
             start_response("404 Not found", [("Content-Type", "text/plain")])
             return [b"key not found"]
     else:
         # key found
+        """
         if env["REQUEST_METHOD"] == "PUT":
             start_response("409 Conflict", [("Content-Type", "text/plain")])
             return [b"key already exists"]
 
-        meta = json.loads(metakey)
-        volume = meta["volume"]
+        """
+        meta = json.loads(metakey.decode("utf-8"))
 
+    volume = meta["volume"]
     # send the redirect for either GET or DELETE
-    headers = [("location", "http://%s%s" % (volume, key)), ("expires", "0")]
-    start_response("302 Found", headers)
+    headers = [("Location", "http://%s%s" % (volume, key))]
+    start_response("307 Temporary Redirect", headers)
     return [b""]
 
 
 class FileCache(object):
     def __init__(self, basedir):
         self.basedir = os.path.realpath(basedir)
-        os.makedirs(basedir, exist_ok=True)
+        os.makedirs(self.basedir, exist_ok=True)
 
         print(f"Filecache in {self.basedir}")
 
@@ -97,15 +100,22 @@ def volume(env, start_response):
     if env["REQUEST_METHOD"] == "GET":
         if not fc.exists(hkey):
             # key not in file cache
-            start_response("404 Not found", [("Content-Type", "text/html")])
+            start_response("404 Not found", [("Content-Type", "text/plain")])
             return [b"key not found"]
 
-        start_response("302 Found", [("Content-Type", "text/html")])
+        start_response("302 Found", [("Content-Type", "text/plain")])
         return fc.get(hkey)
 
     if env["REQUEST_METHOD"] == "PUT":
         flen = int(env.get("CONTENT_LENGTH", "0"))
-        fc.put(hkey, env["wsgi.input"].read(flen))
+
+        if flen > 0:
+            fc.put(hkey, env["wsgi.input"].read(flen))
+            start_response("200 OK", [("Content-Type", "text/plain")])
+            return [b""]
+        else:
+            start_response("411 Length Required", [("Content-Type", "text/plain")])
+            return [b""]
 
     if env["REQUEST_METHOD"] == "DELETE":
         fc.delete(hkey)
